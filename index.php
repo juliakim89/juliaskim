77627aedb1ce97fc7a751bc92c775d758ad27c13
juliakim89 <juliakim89@gmail.com>
2015-03-25 00:08:54 -0700

    anchor link transitions

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php include('includes/header.php'); ?>
   
     <?php include('includes/logo.php'); ?>
+
+    <section id="home">
+	    <h1>
+	    	Product Designer + Front-end Developer
+	    </h1>
+    </section>
     
 <?php include('includes/footer.php'); ?>
